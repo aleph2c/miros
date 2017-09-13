1f0114fd60c1b16e756f254271244b60702ea048
Scott Volk <scottvolk@gmail.com>
2017-09-12 21:26:00 -0700

    Adding an example state function.
    The was done to see how the api feels.

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['docs', 'test', 'protoyping', 'investigations']),
+    packages=find_packages(exclude=['docs', 'test', 'plan', 'investigations']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
