aa42a842297874b9ba1159c522024edbccb9bd44
Scott Volk <scottvolk@gmail.com>
2017-10-27 05:52:47 -0700

    Linting the file

--- test/instrumented_hsm_event_processor_trace_test.py
+++ test/instrumented_hsm_event_processor_trace_test.py
@@ -1,35 +1,30 @@
 import pytest
-from miros.event import ReturnStatus, Signal, signals, Event, return_status
-from miros.hsm   import InstrumentedHsmEventProcessor, HsmTopologyException, spy_on
+from miros.event import signals, Event, return_status
+from miros.hsm   import InstrumentedHsmEventProcessor, spy_on
 import pprint
+
+
 def pp(item):
   print("")
   pprint.pprint(item)
 
-signals.append("A")
-signals.append("B")
-signals.append("C")
-signals.append("D")
-signals.append("E")
-signals.append("F")
-signals.append("G")
 
 ################################################################################
 #                               Trace Graph A1                                 #
 ################################################################################
-'''    The following state chart is used to test the spy on topology A
+# The following state chart is used to test the spy on topology A
+#
+#                             +- graph_a1_s1 -+
+#                             |               +-----+
+#                             |               |     a
+#                             |               <-----+
+#                             +---------------+
+#
+# This is used for testing the type A topology in the trans_ method of the HsmEventProcessor
+# class.
+#   * test__trace_topology_a_1 (diagram)
+#   * test__trace_topology_a_2 (diagram)
 
-                            +- graph_a1_s1 -+
-                            |               +-----+
-                            |               |     a
-                            |               <-----+
-                            +---------------+
-
-This is used for testing the type A topology in the trans_ method of the HsmEventProcessor
-class.
-  * test__trace_topology_a_1 (diagram)
-  * test__trace_topology_a_2 (diagram)
-'''
 @spy_on
 def trace_graph_a1_s1(chart, e):
   status = return_status.UNHANDLED
@@ -43,6 +38,7 @@ def trace_graph_a1_s1(chart, e):
     status, chart.temp.fun = return_status.SUPER, chart.top
   return status
 
+
 @pytest.mark.trace
 def test_trace_topology_a_1():
   chart = InstrumentedHsmEventProcessor()
@@ -52,49 +48,49 @@ def test_trace_topology_a_1():
   assert(len(chart.full.trace) == 2)
   start_trace = chart.full.trace[0]
   assert(start_trace.start_state == 'top')
-  assert(start_trace.signal == None)
+  assert(start_trace.signal is None)
   assert(start_trace.end_state == 'trace_graph_a1_s1')
   trans_trace = chart.full.trace[1]
   trans_trace.start_state == 'trace_graph_a1_s1'
   trans_trace.end_state == 'trace_graph_a1_s1'
   trans_trace.signal == 'A'
 
+
 ################################################################################
 #                               Trace Graph G1                                 #
 ################################################################################
-'''           The following state chart is used to test topology G
-
-                  +-------------------------------- g1_s1 --------------+
-   +---g1_s0---+  | e/ print("ultimate hook")                           |
-   |+-g1_s01--+|  |                      +---------g1_s22 ----------+   |
-   ||         ++-----c------------------->                          |   |
-   ||         ||  |                      | +-------g1_s3 ---------+ |   |
-   |+---------+|  | +-------g1_s21----+  | |    +----g1_s32-----+ | |   |
-   +-----------+  | | +--g1_s211-----+|  | |    |  +-g1_s321--+ | | |   |
-                  | | |+-g1_s2111+   ||  | |    |  |          | | | |   |
-                  | | ||         |   ||  | |    |  |          | | | |   |
-                  | | ||         |   |+-------b---->          | | | |   |
-                  | | ||         |   ||  | |    |  |          | | | |   |
-                  | | ||         |   ||  | |    |  |          | | | |   |
-                  | | ||         |   ||  | |  +---->          | | | |   |
-                  | | ||         |   ||  | |  | |  |          | | | |   |
-                  | | |++--------+   ||  | |  | |  |          | | | +-d->
-                  | | +-|------------+|  | |  | |  +----------+ | | |   |
-                  | +---|-------------+  | |  | +---------------+ | |   |
-                  |     |                | +--|-------------------+ |   |
-                  |     +------------a--------+                     |   |
-                  |                      +--------------------------+   |
-                  |                                                     |
-                  +-----------------------------------------------------+
-
-
-This is used for testing the type E topology in the trans_ method of the HsmEventProcessor
-class.
-  * test_trace_topology_g1_1 - start in graph_g1_s211 (diagram -> a)
-  * test_trace_topology_g1_2 - start in graph_g1_s211 (diagram -> b)
-  * test_trace_topology_g1_3 - start in graph_g1_s01  (diagram -> a)
-  * test_trace_topology_g1_4 - start in graph_g1_s321 (diagram -> d)
-'''
+# The following state chart is used to test topology G
+#
+#                   +-------------------------------- g1_s1 --------------+
+#    +---g1_s0---+  | e/ print("ultimate hook")                           |
+#    |+-g1_s01--+|  |                      +---------g1_s22 ----------+   |
+#    ||         ++-----c------------------->                          |   |
+#    ||         ||  |                      | +-------g1_s3 ---------+ |   |
+#    |+---------+|  | +-------g1_s21----+  | |    +----g1_s32-----+ | |   |
+#    +-----------+  | | +--g1_s211-----+|  | |    |  +-g1_s321--+ | | |   |
+#                   | | |+-g1_s2111+   ||  | |    |  |          | | | |   |
+#                   | | ||         |   ||  | |    |  |          | | | |   |
+#                   | | ||         |   |+-------b---->          | | | |   |
+#                   | | ||         |   ||  | |    |  |          | | | |   |
+#                   | | ||         |   ||  | |    |  |          | | | |   |
+#                   | | ||         |   ||  | |  +---->          | | | |   |
+#                   | | ||         |   ||  | |  | |  |          | | | |   |
+#                   | | |++--------+   ||  | |  | |  |          | | | +-d->
+#                   | | +-|------------+|  | |  | |  +----------+ | | |   |
+#                   | +---|-------------+  | |  | +---------------+ | |   |
+#                   |     |                | +--|-------------------+ |   |
+#                   |     +------------a--------+                     |   |
+#                   |                      +--------------------------+   |
+#                   |                                                     |
+#                   +-----------------------------------------------------+
+#
+#
+# This is used for testing the type E topology in the trans_ method of the HsmEventProcessor
+# class.
+#   * test_trace_topology_g1_1 - start in graph_g1_s211 (diagram -> a)
+#   * test_trace_topology_g1_2 - start in graph_g1_s211 (diagram -> b)
+#   * test_trace_topology_g1_3 - start in graph_g1_s01  (diagram -> a)
+#   * test_trace_topology_g1_4 - start in graph_g1_s321 (diagram -> d)
 @spy_on
 def trace_graph_g1_s0(chart, e):
   status = return_status.UNHANDLED
@@ -105,6 +101,7 @@ def trace_graph_g1_s0(chart, e):
   else:
     status, chart.temp.fun = return_status.SUPER, chart.top
   return status
+
 
 @spy_on
 def trace_graph_g1_s01(chart, e):
@@ -118,6 +115,7 @@ def trace_graph_g1_s01(chart, e):
   else:
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s0
   return status
+
 
 @spy_on
 def trace_graph_g1_s1(chart, e):
@@ -133,6 +131,7 @@ def trace_graph_g1_s1(chart, e):
     status, chart.temp.fun = return_status.SUPER, chart.top
   return status
 
+
 @spy_on
 def trace_graph_g1_s21(chart, e):
   status = return_status.UNHANDLED
@@ -146,6 +145,7 @@ def trace_graph_g1_s21(chart, e):
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s1
   return status
 
+
 @spy_on
 def trace_graph_g1_s211(chart, e):
   status = return_status.UNHANDLED
@@ -156,6 +156,7 @@ def trace_graph_g1_s211(chart, e):
   else:
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s21
   return status
+
 
 @spy_on
 def trace_graph_g1_s2111(chart, e):
@@ -170,6 +171,7 @@ def trace_graph_g1_s2111(chart, e):
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s211
   return status
 
+
 @spy_on
 def trace_graph_g1_s22(chart, e):
   status = return_status.UNHANDLED
@@ -183,6 +185,7 @@ def trace_graph_g1_s22(chart, e):
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s1
   return status
 
+
 @spy_on
 def trace_graph_g1_s3(chart, e):
   status = return_status.UNHANDLED
@@ -193,6 +196,7 @@ def trace_graph_g1_s3(chart, e):
   else:
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s22
   return status
+
 
 @spy_on
 def trace_graph_g1_s32(chart, e):
@@ -205,6 +209,7 @@ def trace_graph_g1_s32(chart, e):
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s3
   return status
 
+
 @spy_on
 def trace_graph_g1_s321(chart, e):
   status = return_status.UNHANDLED
@@ -216,6 +221,7 @@ def trace_graph_g1_s321(chart, e):
     status, chart.temp.fun = return_status.SUPER, trace_graph_g1_s32
   return status
 
+
 @pytest.mark.trace
 @pytest.mark.topology_g
 def test_trace_topology_g1_1():
@@ -224,16 +230,16 @@ def test_trace_topology_g1_1():
   chart.dispatch(Event(signal=signals.A))
   pp(chart.full.spy)
   pp(chart.full.trace)
-  #assert(chart.full.spy == expected_behavior)
   assert(len(chart.full.trace) == 2)
   start_trace = chart.full.trace[0]
   assert(start_trace.start_state == 'top')
-  assert(start_trace.signal == None)
+  assert(start_trace.signal is None)
   assert(start_trace.end_state == 'trace_graph_g1_s2111')
   trans_trace = chart.full.trace[1]
   trans_trace.start_state == 'trace_graph_g1_s2111'
   trans_trace.end_state == 'ttrace_graph_g1_s321'
   trans_trace.signal == 'A'
+
 
 @pytest.mark.trace
 @pytest.mark.topology_g
@@ -243,5 +249,5 @@ def test_trace_topology_g1_2():
   chart.dispatch(Event(signal=signals.E))
   pp(chart.full.spy)
   pp(chart.full.trace)
-  #assert(chart.full.spy == expected_behavior)
+  # assert(chart.full.spy == expected_behavior)
   assert(len(chart.full.trace) == 1)
