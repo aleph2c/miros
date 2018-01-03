d0040013b498961e600356e5fc966ca7672560a0
Scott Volk <scottvolk@gmail.com>
2018-01-03 12:02:01 -0800

    Added ability to use foreign hsm objects.
    Also added a timing callback via the pika api, which allows the process
    to be killed; this mechanism will be used in the future to remove the
    polling behavior and have the consumption process killed asynchronously.

--- examples/b_trace_consumer.py
+++ examples/b_trace_consumer.py
@@ -19,6 +19,8 @@
 '''
 import pika
 import socket
+from miros.foreign import ForeignHsm
+from miros.hsm import pp
 
 
 # Taken from Jamieson Becker
@@ -37,9 +39,10 @@ def get_ip():
 
 # create a connection and a channel
 credentials = pika.PlainCredentials('bob', 'dobbs')
-parameters = pika.ConnectionParameters(get_ip(), 5672, '/', credentials)
-connection = pika.BlockingConnection(parameters=parameters)
-channel = connection.channel()
+parameters  = pika.ConnectionParameters(get_ip(), 5672, '/', credentials)
+connection  = pika.BlockingConnection(parameters=parameters)
+channel     = connection.channel()
+count       = 0
 
 # if the queue already exists this call will do nothing
 channel.queue_declare(queue='spy_queue', durable=True)
@@ -47,19 +50,46 @@ channel.queue_declare(queue='trace_queue', durable=True)
 print(' [*] Waiting for message. To exit press CTRL+C')
 
 
+foreign_hsm = ForeignHsm()
+
+
 # create a spy_callback function received messages in the queue
 def spy_callback(ch, method, properties, body):
-  print(" [x] {!s}".format(body.decode('utf8')))
+  global foreign_hsm
+  foreign_spy_item = body.decode('utf8')
+  foreign_hsm.append_to_spy(foreign_spy_item)
+  print(" [x] {!s}".format(foreign_spy_item))
   ch.basic_ack(delivery_tag = method.delivery_tag)
 
 
 # create a trace_callback function received messages in the queue
 def trace_callback(ch, method, properties, body):
-  print(" [x] Trace: {!s}".format(body.decode('utf8')))
+  global foreign_hsm
+  pp(ch)
+  foreign_trace_item = body.decode('utf8')
+  foreign_hsm.append_to_trace(foreign_trace_item)
+  print(" [x] Trace: {!s}".format(foreign_trace_item))
   ch.basic_ack(delivery_tag = method.delivery_tag)
 
 
-channel.basic_qos(prefetch_count=1)
+def one_second_callback():
+  global count
+  global foreign_hsm
+  global channel
+  pp(foreign_hsm.spy())
+  print(foreign_hsm.trace())
+  # stop processing or reconnect this callback to a timer
+  count += 1
+  if count >= 3:
+    channel.stop_consuming()
+  else:
+    connection.add_timeout(deadline=10, callback_method=one_second_callback)
+    foreign_hsm.clear_spy()
+    foreign_hsm.clear_trace()
+
+
+connection.add_timeout(deadline=1, callback_method=one_second_callback)
+channel.basic_qos(prefetch_count=10)
 
 # register the spy_callback with a queue
 channel.basic_consume(spy_callback,   queue='spy_queue')
@@ -67,3 +97,4 @@ channel.basic_consume(trace_callback, queue='trace_queue')
 
 # patiently wait forever
 channel.start_consuming()
+
