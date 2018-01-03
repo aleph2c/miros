022750422c77031e16d4628b00a725936f94aa94
Scott Volk <scottvolk@gmail.com>
2018-01-03 11:59:55 -0800

    First working foriegn module.
    It provides a spy/trace interface for an HSM working on a different
    machine.  The naming might be changed in the future.

--- miros/foreign.py
+++ miros/foreign.py
@@ -1,15 +1,36 @@
-class Hsm():
+from collections  import deque
+from miros.hsm import HsmEventProcessor, Attribute
+
+
+class ForeignHsm():
   '''
   Provides an object that can be filled with the trace/spy information coming
   from another statechart on a different host.
   '''
   def __init__(self):
-    self.foreign_trace = []
-    self.foreign_spy = []
+    self.foreign = Attribute()
+    self.foreign.spy = deque(maxlen=HsmEventProcessor.SPY_RING_BUFFER_SIZE)
+    self.foreign.trace = deque(maxlen=HsmEventProcessor.TRC_RING_BUFFER_SIZE)
+
+  def clear_spy(self):
+    self.foreign.spy.clear()
+
+  def clear_trace(self):
+    self.foreign.trace.clear()
 
   def trace(self):
-    return self.foreign_trace
+    strace = ""
+    for tr in self.foreign.trace:
+      strace += tr
+      strace += "\n"
+    return strace
 
   def spy(self):
-    return self.foreign_spy
-    
+    return list(self.foreign.spy)
+
+  def append_to_spy(self, item):
+    self.foreign.spy.append(item)
+
+  def append_to_trace(self, item):
+    self.foreign.trace.append(item)
+
