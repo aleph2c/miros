bc54d7bce5b15b6a1271fa5c5f6874cf074dc4b8
Scott Volk <scottvolk@gmail.com>
2018-01-09 08:08:16 -0800

    Ran code through a lint tool
    Python syntax nazis rejoice.

--- examples/c_trace_producer.py
+++ examples/c_trace_producer.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python3
 '''
   Usage:
-    Sends a new task request to "c_trace_consumer.py"
+    Sends a new task request to "c_trace_consumer.py" running on a different
+    machine.
+
     1) start up a couple of c_trace_consumer.py sessions
 
-    > python3 c_trace_consumer.py message....
+    > python3 c_trace_producer.py
 
   Demonstrates:
     * Exchanges
     * 'fanout' routing
     * Work queues
+    * Sending encrypted spy/trace information to another computer
 
   Monitoring:
     * to view the results with the RabbitMQ manager:
     <hostname>:15672 in browser
 
-    where <hostname> is the results of your hostname command
+    Where <hostname> is the results of your `hostname` command
 
 '''
 
 import pika
 from cryptography.fernet import Fernet
-import sys
-import os.path, sys
 from miros.activeobject import Factory
 from miros.event import signals, Event, return_status
 from miros.hsm import pp
 import time
 from functools import wraps
 
+
 class RabbitProducer(Factory):
   '''
   The networking side of the RabbitProducer looks like this:
-                
+
   |--- RabbitProducer ---------------------->|
 
                             +----------------+  +---------+
   +------------------+   +->| spy exchange   +->| spy_c   |
   |                  |   |  +----------------+  +---------+
-  | startchart emit  +-->|                       
-  |                  |   |                      
+  | startchart emit  +-->|
+  |                  |   |
   +------------------+   |  +----------------+  +---------+
                          +->| trace exchange +->| trace_c |
                             +----------------+  +---------+
@@ -48,7 +50,7 @@ class RabbitProducer(Factory):
   object.   The live spy/trace output are encrypted then routed into two
   separate exchanges, which transmit their messaging to the ``spy_c` and
   ``trace_c`` consumers, running on a different machine (see c_trace_consumer.py
-  for this code). 
+  for this code).
 
   To build a chart with this rabbitmq producer you would slightly adjust how
   you build a factory:
@@ -58,17 +60,17 @@ class RabbitProducer(Factory):
           rabbit_password="dobbs",
           ip="192.168.1.72",
           port=5672)
-  
+
   The chart would have a typical factory api, and with it you could build and
   start your statechart.
 
   To turn off the networking:
-      
+
       chart.connection.close()
 
   '''
   def __init__(self, chart_name, rabbit_user, rabbit_password, ip, port):
-    super().__init__(chart_name+'_'+ip)
+    super().__init__(chart_name + '_' + ip)
     self.rabbit_user = rabbit_user
     self.rabbit_password = rabbit_password
     self.destination_ip = ip
@@ -77,7 +79,7 @@ class RabbitProducer(Factory):
     credentials = pika.PlainCredentials(rabbit_user, rabbit_password)
     parameters = pika.ConnectionParameters(ip, port, '/', credentials)
     self.connection = pika.BlockingConnection(parameters=parameters)
-    
+
     self.channel = self.connection.channel()
     self.channel.exchange_declare(exchange='spy', exchange_type='fanout')
     self.channel.exchange_declare(exchange='trace', exchange_type='fanout')
@@ -102,18 +104,20 @@ class RabbitProducer(Factory):
 
     @encrypt
     def broadcast_spy(spy_live):
-      self.channel.basic_publish(exchange='spy',
+      self.channel.basic_publish(
+          exchange='spy',
           routing_key='',
           body=spy_live
-          )
+      )
 
     @strip_trace
     @encrypt
     def broadcast_trace(trace_live):
-      self.channel.basic_publish(exchange='trace',
+      self.channel.basic_publish(
+          exchange='trace',
           routing_key='',
           body=trace_live
-          )
+      )
 
     self.register_live_spy_callback(broadcast_spy)
     self.register_live_trace_callback(broadcast_trace)
@@ -125,70 +129,82 @@ def producer_outer_entry(chart, e):
   status = return_status.UNHANDLED
   return status
 
+
 def producer_outer_exit(chart, e):
   status = return_status.UNHANDLED
   return status
+
 
 def producer_outer_init(chart, e):
   status = return_status.UNHANDLED
   return status
 
+
 def producer_outer_B(chart, e):
   status = chart.trans(producer_outer)
   return status
+
 
 def c1_entry(chart, e):
   status = return_status.UNHANDLED
   return status
 
+
 def c1_exit(chart, e):
   status = return_status.UNHANDLED
   return status
+
 
 def c1_A(chart, e):
   status = return_status.UNHANDLED
   return status
 
+
 def c2_entry(chart, e):
   status = return_status.UNHANDLED
   return status
+
 
 def c2_exit(chart, e):
   status = return_status.UNHANDLED
   return status
 
+
 def c2_A(chart, e):
   status = return_status.UNHANDLED
   return status
 
-chart = RabbitProducer(chart_name="rabbit_producer",
-    rabbit_user="bob",
-    rabbit_password="dobbs",
-    ip="192.168.1.72",
-    port=5672)
+
+chart = RabbitProducer(
+  chart_name="rabbit_producer",
+  rabbit_user="bob",
+  rabbit_password="dobbs",
+  ip="192.168.1.72",
+  port=5672
+)
 
 producer_outer = chart.create(state='producer_outer'). \
-    catch(signal=signals.ENTRY_SIGNAL, handler=producer_outer_entry). \
-    catch(signal=signals.EXIT_SIGNAL, handler=producer_outer_exit). \
-    catch(signal=signals.INIT_SIGNAL, handler=producer_outer_init). \
-    catch(signal=signals.B, handler=producer_outer_B). \
-    to_method()
+  catch(signal=signals.ENTRY_SIGNAL, handler=producer_outer_entry). \
+  catch(signal=signals.EXIT_SIGNAL, handler=producer_outer_exit). \
+  catch(signal=signals.INIT_SIGNAL, handler=producer_outer_init). \
+  catch(signal=signals.B, handler=producer_outer_B). \
+  to_method()
 
 c1 = chart.create(state='c1'). \
-    catch(signal=signals.ENTRY_SIGNAL, handler=c1_entry). \
-    catch(signal=signals.EXIT_SIGNAL, handler=c1_exit). \
-    catch(signal=signals.A, handler=c1_A). \
-    to_method()
+  catch(signal=signals.ENTRY_SIGNAL, handler=c1_entry). \
+  catch(signal=signals.EXIT_SIGNAL, handler=c1_exit). \
+  catch(signal=signals.A, handler=c1_A). \
+  to_method()
 
 c2 = chart.create(state='c2'). \
-    catch(signal=signals.ENTRY_SIGNAL, handler=c2_entry). \
-    catch(signal=signals.EXIT_SIGNAL, handler=c2_exit). \
-    catch(signal=signals.A, handler=c2_A). \
-    to_method()
+  catch(signal=signals.ENTRY_SIGNAL, handler=c2_entry). \
+  catch(signal=signals.EXIT_SIGNAL, handler=c2_exit). \
+  catch(signal=signals.A, handler=c2_A). \
+  to_method()
 
 chart.nest(producer_outer, parent=None). \
-    nest(c1, parent=producer_outer). \
-    nest(c2, parent=producer_outer)
+  nest(c1, parent=producer_outer). \
+  nest(c2, parent=producer_outer)
 
 chart.start_at(producer_outer)
 chart.post_fifo(Event(signal=signals.B))
