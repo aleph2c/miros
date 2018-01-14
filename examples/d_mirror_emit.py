8c2879f2eda0dfc77ff7307194cb49f190a53848
Scott Volk <scottvolk@gmail.com>
2018-01-14 08:08:12 -0800

    Getting ready to put tx/rx into one file.

--- examples/d_mirror_emit.py
+++ examples/d_mirror_emit.py
@@ -1,48 +1,10 @@
 #!/usr/bin/env python3
-import pika
 import sys
+import pika
+import socket
 from types import SimpleNamespace
 
-'''
-  Usage:
-    Sends a routed message (set from cli) to "d_receive_logs_direct.py"
-    1) start up a couple of d_receive_logs_direct.py sessions with different
-       routes:
-    > python3 d_receive_logs_direct.py info warning error
-    > python3 d_receive_logs_direct.py error
-
-    2) send routed messages:
-
-    > python3 d_emit_log_direct.py error all_should_see_this
-    > python3 d_emit_log_direct.py info first_should_see_this
-
-
-  Demonstrates:
-    * Publish Subscribe
-    * Direct exchange
-
-
-    * to view the results with the RabbitMQ manager:
-    <hostname>:15672 in browser
-
-    where <hostname> is the results of your hostname command
-
-'''
-import socket
-
-class Connections():
-  def __init__(self, base, start, end, user, password):
-    self.possible_ips = [base+str(i) for i in [*range(start,end)]]
-    targets = Connections.scout_targets(self.possible_ips, user, password)
-    self.channels = Connections.get_channels(self.possible_ips, user, password)
-
-  def message_to_other_channels(self, message):
-    for channel in self.channels:
-      ip = channel.extension.ip_address
-      # find target in channel
-      channel.basic_publish(exchange='mirror',
-          routing_key=ip, body=message)
-      print(" [x] Sent \"{}\" to {}".format(message, ip))
+class Connection():
 
   @staticmethod
   def get_ip():
@@ -57,16 +19,36 @@ class Connections():
     return ip
 
   @staticmethod
+  def get_blocking_connection(user, password,  ip, port):
+    credentials = pika.PlainCredentials(user, password)
+    parameters = pika.ConnectionParameters(ip, port, '/', credentials)
+    connection = pika.BlockingConnection(parameters=parameters)
+    return connection
+
+
+class EmitConnections():
+
+  def __init__(self, base, start, end, user, password):
+    possible_ips  = [base + str(i) for i in [*range(start, end + 1)]]
+    targets       = EmitConnections.scout_targets(possible_ips, user, password)
+    self.channels = EmitConnections.get_channels(targets, user, password)
+
+  def message_to_other_channels(self, message):
+    for channel in self.channels:
+      ip = channel.extension.ip_address
+      channel.basic_publish(exchange='mirror',
+          routing_key=ip, body=message)
+      print(" [x] Sent \"{}\" to {}".format(message, ip))
+
+  @staticmethod
   def scout_targets(targets, user, password):
     possible_targets = targets[:]
-    possible_targets.remove(Connections.get_ip())
-    credentials = pika.PlainCredentials(user, password)
+    possible_targets.remove(Connection.get_ip())
     message = 'discovery probe'
 
     for target in possible_targets:
-      parameters = pika.ConnectionParameters(target, 5672, '/', credentials)
       try:
-        connection = pika.BlockingConnection(parameters=parameters)
+        connection = Connection.get_blocking_connection(user, password, target, 5672)
         channel = connection.channel()
         channel.exchange_declare(exchange='mirror', exchange_type='direct')
         channel.basic_publish(exchange='mirror', routing_key=target, body=message)
@@ -79,11 +61,9 @@ class Connections():
   @staticmethod
   def get_channels(targets, user, password):
     channels = []
-    credentials = pika.PlainCredentials(user, password)
     for target in targets:
-      parameters = pika.ConnectionParameters(target, 5672, '/', credentials)
       try:
-        connection = pika.BlockingConnection(parameters=parameters)
+        connection = Connection.get_blocking_connection(user, password, target, 5672)
         channel = connection.channel()
         channel.exchange_declare(exchange='mirror', exchange_type='direct')
         channel.extension = SimpleNamespace()
@@ -93,5 +73,6 @@ class Connections():
         pass
     return channels
 
-rabbit = Connections(base="192.168.1.", start=70, end=80, user="bob", password="dobbs")
-rabbit.message_to_other_channels("an actual message")
+
+output = EmitConnections(base="192.168.1.", start=70, end=73, user="bob", password="dobbs")
+output.message_to_other_channels("an actual message")
