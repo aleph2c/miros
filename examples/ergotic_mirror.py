c53c7090dc114828584c8771211490aa94d0c421
Scott Volk <scottvolk@gmail.com>
2018-01-14 16:38:02 -0800

    Added rx callback support and docstrings

--- examples/ergotic_mirror.py
+++ examples/ergotic_mirror.py
@@ -8,6 +8,10 @@ from types import SimpleNamespace
 from threading import Thread, Event
 
 class Connection():
+  '''
+  A grab bag of networking static methods used by different objects within this
+  module
+  '''
 
   @staticmethod
   def get_ip():
@@ -30,6 +34,14 @@ class Connection():
     return connection
 
 class ReceiveConnections():
+  '''
+  Receives connections on this ip address from port 5672
+  It creates a 'mirror' exchange using direct routing where the routing key is
+  this ip address as a string.
+
+  The interface to this class should be done through the Receiver
+
+  '''
   def __init__(self, user, password):
     self.connection = Connection.get_blocking_connection(user, password, Connection.get_ip(), 5672)
     self.channel = self.connection.channel()
@@ -41,17 +53,33 @@ class ReceiveConnections():
 
     self.task_run_event = Event()
     self.task_run_event.set()
-
+    self.live_callback = self.default_callback
     print(' [x] Waiting for messages. To exit press CTRL-C')
     def callback(ch, method, properties, body):
-      self.callback(ch, method, properties, body)
+      self.live_callback(ch, method, properties, body)
     self.channel.basic_consume(callback, queue=self.queue_name, no_ack=True)
 
-  def callback(self, ch, method, properties, body):
+  def default_callback(self, ch, method, properties, body):
     print(" [x] {}:{}".format(method.routing_key, body.decode('utf8')))
 
+  def register_live_callback(self, live_callback):
+    self.live_callback = live_callback
+
   def start_consuming(self):
+    # We make a thread so that the channel.start_consuming doesn't steal our
+    # program control.
+
+    # The thread has it's own timeout callback (pika) which wakes up every 10
+    # seconds and checks the self.trask_run_event, which is controlled outside
+    # of the thread.  If it is set, it continues to work, if it isn't set it
+    # stop the pika consumer and the thread dies.
     def channel_consumer(self):
+      '''
+      This timeout_callback is the only way to communicate with a pika channel
+      once it has started consuming.  This time out checks to see if this
+      thread should die, if so, it calls stop_consuming, if not, it arms
+      another timeout callback.  (Working with the library)
+      '''
       self.task_run_event.set()
       def timeout_callback():
         if self.task_run_event.is_set():
@@ -59,16 +87,34 @@ class ReceiveConnections():
         else:
           self.channel.stop_consuming()
           return
+
+      # We are within our own thread, we arm a timeout callback
       self.connection.add_timeout(deadline=10, callback_method=timeout_callback)
+      # This process will block forever, with the exception of calling the
+      # timeout_callback every 10 seconds.
       self.channel.start_consuming()
+
+    # Create and start the thread.  The thread can be stopped by clearing the
+    # task_run_event Event.
     thread = Thread(target=channel_consumer, args=(self,), daemon=True)
     thread.start()
 
   def stop_consuming(self):
+    '''
+    This will kill the channel_consumer within the next 10 seconds
+    '''
     self.task_run_event.clear()
 
 class EmitConnections():
+  '''
+  Scouts a range of ip addresses, creates a 'mirror' exchange which can dispatch
+  messages to any rabbitmq server it has detected in the ip range.  It uses a
+  direct routing strategy where the routing_key is the ip address of the node it
+  wishes to communicate with.
 
+  This class should be accessed through the Transmitter object
+
+  '''
   def __init__(self, base, start, end, user, password):
     possible_ips  = [base + str(i) for i in [*range(start, end + 1)]]
     targets       = EmitConnections.scout_targets(possible_ips, user, password)
@@ -115,6 +161,23 @@ class EmitConnections():
     return channels
 
 class Receiver():
+  '''
+  Creates a rabbitmq receiver.  You can register a live callback which will be
+  called when a message is received, then start consuming.  You can stop
+  consuming and start consuming at a later time.
+
+  Example:
+    import time
+    def custom_rx_callback(ch, method, properties, body):
+        print(" [+] {}:{}".format(method.routing_key, body.decode('utf8')))
+
+    rx = Receiver(user='bob', password='dobbs')
+    rx.register_live_callback(custom_rx_callback)
+    rx.start_consuming() # launches a consuming task
+    time.sleep(10)
+    rx.stop_consuming()  # kills consuming task
+    rx.start_consuming() # launches a consuming task with same custom_rx_callback
+  '''
   def __init__(self, user, password):
     self.user     = user
     self.password = password
@@ -123,13 +186,30 @@ class Receiver():
   def start_consuming(self):
     self.rx.start_consuming()
 
+  def register_live_callback(self, live_callback):
+    self.live_callback = live_callback
+    self.rx.register_live_callback(live_callback)
+
   def stop_consuming(self):
     self.rx.stop_consuming()
     del(self.rx)
     self.rx = ReceiveConnections(self.user, self.password)
+    # re-register our live callback with the next instantiation
+    if hasattr(self, 'live_callback') is True:
+      if self.live_callback is not None:
+        self.rx.register_live_callback(self.live_callback)
 
 class Transmitter():
+  '''
+  Scans addresses 192.168.1.70-192.168.1.73 looking for rabbitmq receivers.  If
+  it finds them, they will be accessible through the message_to_other_channels
+  method provided by the class.
 
+  Example:
+    tx = Transmitter(user="bob", password="dobbs")
+    tx.message_to_other_channels("an actual message")
+
+  '''
   def __init__(self, user, password):
     self.tx = EmitConnections(base="192.168.1.",
                 start=70,
@@ -140,20 +220,22 @@ class Transmitter():
   def message_to_other_channels(self, message):
     self.tx.message_to_other_channels(message)
 
-
 tranceiver_type = sys.argv[1:]
 if not tranceiver_type:
   sys.stderr.write("Usage: {} [rx]/[tx]\n".format(sys.argv[0]))
 
+def custom_rx_callback(ch, method, properties, body):
+    print(" [+] {}:{}".format(method.routing_key, body.decode('utf8')))
+
 if tranceiver_type[0] == 'rx':
   rx = Receiver('bob', 'dobbs')
+  rx.register_live_callback(custom_rx_callback)
   rx.start_consuming()
   time.sleep(10)
   rx.stop_consuming()
   rx.start_consuming()
   time.sleep(10)
   rx.stop_consuming()
-
 elif tranceiver_type[0] == 'tx':
   tx = Transmitter(user="bob", password="dobbs")
   tx.message_to_other_channels("an actual message")
