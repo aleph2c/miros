d68376385f58e4e90e09e0717fcd1356f4b06269
Scott Volk <scottvolk@gmail.com>
2018-03-18 08:47:17 -0700

    Adjusted so that Event will deserialize

--- examples/mesh_network.py
+++ examples/mesh_network.py
@@ -347,8 +347,12 @@ class NetworkTool():
       except ValueError:
         plain_text = p_plain_text
 
-      if isinstance(plain_text, Event):
+      try:
         plain_text = Event.loads(plain_text)
+      except:
+        print("failing")
+        pass
+
       fn(ch, method, properties, plain_text)
     return _pickle_loads
 
