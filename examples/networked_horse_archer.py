f6bd2fdcf9df102773f1e92c9d269ec38b6d04b2
Scott Volk <scottvolk@gmail.com>
2018-04-06 10:16:15 -0700

    compressing time to 10:1

--- examples/networked_horse_archer.py
+++ examples/networked_horse_archer.py
@@ -720,7 +720,7 @@ archer.nest(battle, parent=None). \
 
 if __name__ == '__main__':
   print("I am {}".format(archer.name))
-  archer.time_compression = 5
+  archer.time_compression = 10
   archer.start_at(battle)
 
   snoop_type = sys.argv[1:]
