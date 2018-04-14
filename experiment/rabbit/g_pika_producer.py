43606fd208451132056bc77782e9a303fbea1d52
Scott Volk <scottvolk@gmail.com>
2018-04-14 07:26:38 -0700

    Slight adjustment to docstrings.

--- experiment/rabbit/g_pika_producer.py
+++ experiment/rabbit/g_pika_producer.py
@@ -28,7 +28,7 @@ class PID():
     self.dt    = dt
 
   def next(self, x):
-    """A simple PID"""
+    '''A simple PID'''
     # err[0] = ref - x
     # err[1] = err[0] from the last sample
     # int[0] = int[1] + err[0]
@@ -71,8 +71,8 @@ class QueueToSampleTimeControl(PID):
       self.min_tempo = 0.000001
 
   def next(self, x):
-    """Invert the output of our PID -> large amounts of control need to express
-       short durations in time"""
+    '''Invert the output of our PID -> large amounts of control need to express
+       short durations in time'''
     output = super().next(x)
 
     # if the controller is working accelerate the wind-down of the integrator
@@ -98,7 +98,7 @@ class QueueToSampleTimeControl(PID):
     return time_recommendation
 
 class SimplePikaTopicPublisher():
-  """
+  '''
   This is a pika (Python-RabbitMq) message publisher heavily based on the
   asychronous example provided in the pika documentation.  It should handle
   unexpected interactions with RabbitMQ such as channel and connection closures.
@@ -151,7 +151,7 @@ class SimplePikaTopicPublisher():
     network and the queue sampler callback's frequency will temporarily
     increase to deal with queue bursting.
 
-  """
+  '''
   EXCHANGE_TYPE             = 'topic'
   PUBLISH_FAST_INTERVAL_SEC = 0.000001  # fast
   PRODUCER_VERSION          = u'1.0'
@@ -162,12 +162,12 @@ class SimplePikaTopicPublisher():
                publish_tempo_sec,
                exchange_name,
                queue_name):
-    """Setup the example publisher object, passing in the URL we will use
+    '''Setup the example publisher object, passing in the URL we will use
     to connect to RabbitMQ.
 
     :param str amqp_url: The URL for connecting to RabbitMQ
 
-    """
+    '''
     self._connection = None
     self._channel = None
 
@@ -194,7 +194,7 @@ class SimplePikaTopicPublisher():
     self._rabbit_routing_key = routing_key
 
   def connect(self):
-    """This method connects to RabbitMQ, returning the connection handle.
+    '''This method connects to RabbitMQ, returning the connection handle.
     When the connection is established, the on_connection_open method
     will be invoked by pika. If you want the reconnection to work, make
     sure you set stop_ioloop_on_close to False, which is not the default
@@ -202,34 +202,34 @@ class SimplePikaTopicPublisher():
 
     :rtype: pika.SelectConnection
 
-    """
+    '''
     LOGGER.info('Connecting to %s', self._amqp_url)
     return pika.SelectConnection(pika.URLParameters(self._amqp_url),
                    self.on_connection_open,
                    stop_ioloop_on_close=False)
 
   def on_connection_open(self, unused_connection):
-    """This method is called by pika once the connection to RabbitMQ has
+    '''This method is called by pika once the connection to RabbitMQ has
     been established. It passes the handle to the connection object in
     case we need it, but in this case, we'll just mark it unused.
 
     :type unused_connection: pika.SelectConnection
 
-    """
+    '''
     LOGGER.info('Connection opened')
     self.add_on_connection_close_callback()
     self.open_channel()
 
   def add_on_connection_close_callback(self):
-    """This method adds an on close callback that will be invoked by pika
+    '''This method adds an on close callback that will be invoked by pika
     when RabbitMQ closes the connection to the publisher unexpectedly.
 
-    """
+    '''
     LOGGER.info('Adding connection close callback')
     self._connection.add_on_close_callback(self.on_connection_closed)
 
   def on_connection_closed(self, connection, reply_code, reply_text):
-    """This method is invoked by pika when the connection to RabbitMQ is
+    '''This method is invoked by pika when the connection to RabbitMQ is
     closed unexpectedly. Since it is unexpected, we will reconnect to
     RabbitMQ if it disconnects.
 
@@ -237,7 +237,7 @@ class SimplePikaTopicPublisher():
     :param int reply_code: The server provided reply_code if given
     :param str reply_text: The server provided reply_text if given
 
-    """
+    '''
     self._channel = None
     if self._closing:
       self._connection.ioloop.stop()
@@ -247,10 +247,10 @@ class SimplePikaTopicPublisher():
       self._connection.add_timeout(5, self.reconnect)
 
   def reconnect(self):
-    """Will be invoked by the IOLoop timer if the connection is
+    '''Will be invoked by the IOLoop timer if the connection is
     closed. See the on_connection_closed method.
 
-    """
+    '''
     self._deliveries = []
     self._acked = 0
     self._nacked = 0
@@ -266,39 +266,39 @@ class SimplePikaTopicPublisher():
     self._connection.ioloop.start()
 
   def open_channel(self):
-    """This method will open a new channel with RabbitMQ by issuing the
+    '''This method will open a new channel with RabbitMQ by issuing the
     Channel.Open RPC command. When RabbitMQ confirms the channel is open
     by sending the Channel.OpenOK RPC reply, the on_channel_open method
     will be invoked.
 
-    """
+    '''
     LOGGER.info('Creating a new channel')
     self._connection.channel(on_open_callback=self.on_channel_open)
 
   def on_channel_open(self, channel):
-    """This method is invoked by pika when the channel has been opened.
+    '''This method is invoked by pika when the channel has been opened.
     The channel object is passed in so we can make use of it.
 
     Since the channel is now open, we'll declare the exchange to use.
 
     :param pika.channel.Channel channel: The channel object
 
-    """
+    '''
     LOGGER.info('Channel opened')
     self._channel = channel
     self.add_on_channel_close_callback()
     self.setup_exchange(self._rabbit_exchange_name)
 
   def add_on_channel_close_callback(self):
-    """This method tells pika to call the on_channel_closed method if
+    '''This method tells pika to call the on_channel_closed method if
     RabbitMQ unexpectedly closes the channel.
 
-    """
+    '''
     LOGGER.info('Adding channel close callback')
     self._channel.add_on_close_callback(self.on_channel_closed)
 
   def on_channel_closed(self, channel, reply_code, reply_text):
-    """Invoked by pika when RabbitMQ unexpectedly closes the channel.
+    '''Invoked by pika when RabbitMQ unexpectedly closes the channel.
     Channels are usually closed if you attempt to do something that
     violates the protocol, such as re-declare an exchange or queue with
     different parameters. In this case, we'll close the connection
@@ -308,47 +308,47 @@ class SimplePikaTopicPublisher():
     :param int reply_code: The numeric reason the channel was closed
     :param str reply_text: The text reason the channel was closed
 
-    """
+    '''
     LOGGER.warning('Channel was closed: (%s) %s', reply_code, reply_text)
     if not self._closing:
       self._connection.close()
 
   def setup_exchange(self, exchange_name):
-    """Setup the exchange on RabbitMQ by invoking the Exchange.Declare RPC
+    '''Setup the exchange on RabbitMQ by invoking the Exchange.Declare RPC
     command. When it is complete, the on_exchange_declareok method will
     be invoked by pika.
 
     :param str|unicode exchange_name: The name of the exchange to declare
 
-    """
+    '''
     LOGGER.info('Declaring exchange %s', exchange_name)
     self._channel.exchange_declare(self.on_exchange_declareok,
                      exchange_name,
                      self.EXCHANGE_TYPE)
 
   def on_exchange_declareok(self, unused_frame):
-    """Invoked by pika when RabbitMQ has finished the Exchange.Declare RPC
+    '''Invoked by pika when RabbitMQ has finished the Exchange.Declare RPC
     command.
 
     :param pika.Frame.Method unused_frame: Exchange.DeclareOk response frame
 
-    """
+    '''
     LOGGER.info('Exchange declared')
     self.setup_queue(self._rabbit_queue_name)
 
   def setup_queue(self, queue_name):
-    """Setup the queue on RabbitMQ by invoking the Queue.Declare RPC
+    '''Setup the queue on RabbitMQ by invoking the Queue.Declare RPC
     command. When it is complete, the on_queue_declareok method will
     be invoked by pika.
 
     :param str|unicode queue_name: The name of the queue to declare.
 
-    """
+    '''
     LOGGER.info('Declaring queue %s', queue_name)
     self._channel.queue_declare(self.on_queue_declareok, queue_name)
 
   def on_queue_declareok(self, method_frame):
-    """Method invoked by pika when the Queue.Declare RPC call made in
+    '''Method invoked by pika when the Queue.Declare RPC call made in
     setup_queue has completed. In this method we will bind the queue
     and exchange together with the routing key by issuing the Queue.Bind
     RPC command. When this command is complete, the on_bindok method will
@@ -356,30 +356,30 @@ class SimplePikaTopicPublisher():
 
     :param pika.frame.Method method_frame: The Queue.DeclareOk frame
 
-    """
+    '''
     LOGGER.info('Binding %s to %s with %s',
           self._rabbit_exchange_name, self._rabbit_queue_name, self._rabbit_routing_key)
     self._channel.queue_bind(self.on_bindok, self._rabbit_queue_name,
                  self._rabbit_exchange_name, self._rabbit_routing_key)
 
   def on_bindok(self, unused_frame):
-    """This method is invoked by pika when it receives the Queue.BindOk
+    '''This method is invoked by pika when it receives the Queue.BindOk
     response from RabbitMQ. Since we know we're now setup and bound, it's
-    time to start publishing."""
+    time to start publishing.'''
     LOGGER.info('Queue bound')
     self.start_publishing()
 
   def start_publishing(self):
-    """This method will enable delivery confirmations and schedule the
+    '''This method will enable delivery confirmations and schedule the
     first message to be sent to RabbitMQ
 
-    """
+    '''
     LOGGER.info('Issuing consumer related RPC commands')
     self.enable_delivery_confirmations()
     self.schedule_next_producer_heart_beat(self._publish_tempo_sec)
 
   def enable_delivery_confirmations(self):
-    """Send the Confirm.Select RPC method to RabbitMQ to enable delivery
+    '''Send the Confirm.Select RPC method to RabbitMQ to enable delivery
     confirmations on the channel. The only way to turn this off is to close
     the channel and create a new one.
 
@@ -388,12 +388,12 @@ class SimplePikaTopicPublisher():
     or Basic.Nack method from RabbitMQ that will indicate which messages it
     is confirming or rejecting.
 
-    """
+    '''
     LOGGER.info('Issuing Confirm.Select RPC command')
     self._channel.confirm_delivery(self.on_delivery_confirmation)
 
   def on_delivery_confirmation(self, method_frame):
-    """Invoked by pika when RabbitMQ responds to a Basic.Publish RPC
+    '''Invoked by pika when RabbitMQ responds to a Basic.Publish RPC
     command, passing in either a Basic.Ack or Basic.Nack frame with
     the delivery tag of the message that was published. The delivery tag
     is an integer counter indicating the message number that was sent
@@ -404,7 +404,7 @@ class SimplePikaTopicPublisher():
 
     :param pika.frame.Method method_frame: Basic.Ack or Basic.Nack frame
 
-    """
+    '''
     confirmation_type = method_frame.method.NAME.split('.')[1].lower()
     LOGGER.info('Received %s for delivery tag: %i',
           confirmation_type,
@@ -428,10 +428,10 @@ class SimplePikaTopicPublisher():
       LOGGER.info('Received delivery tag for something we did not send')
 
   def schedule_next_producer_heart_beat(self, timeout):
-    """If we are not closing our connection to RabbitMQ, schedule another
+    '''If we are not closing our connection to RabbitMQ, schedule another
     message to be delivered in self._publish_tempo_sec seconds.
 
-    """
+    '''
     if self._stopping:
       return
     # Scheduling next Task queue check
@@ -439,7 +439,7 @@ class SimplePikaTopicPublisher():
     self._connection.add_timeout(timeout, self.producer_heart_beat)
 
   def publish_message(self, message):
-    """If the class is not stopping, publish a message to RabbitMQ,
+    '''If the class is not stopping, publish a message to RabbitMQ,
     appending a list of deliveries with the message number that was sent.
     This list will be used to check for delivery confirmations in the
     on_delivery_confirmations method.
@@ -453,7 +453,7 @@ class SimplePikaTopicPublisher():
 
       # then load it into a timer
       self._connection.add_timeout(self.PUBLISH_FAST_INTERVAL_SEC, cb)
-    """
+    '''
     if self._stopping:
       return
     properties = pika.BasicProperties(app_id='miros-rabbitmq-publisher',
@@ -469,25 +469,25 @@ class SimplePikaTopicPublisher():
     LOGGER.info('Published message # %i', self._message_number)
 
   def close_channel(self):
-    """Invoke this command to close the channel with RabbitMQ by sending
-    the Channel.Close RPC command."""
+    '''Invoke this command to close the channel with RabbitMQ by sending
+    the Channel.Close RPC command.'''
     LOGGER.info('Closing the channel')
     if self._channel:
       self._channel.close()
 
   def run(self):
-    """Run the example code by connecting and then starting the IOLoop. """
+    '''Run the example code by connecting and then starting the IOLoop. '''
     self._connection = self.connect()
     self._connection.ioloop.start()
 
   def stop(self):
-    """Stop the example by closing the channel and connection and releasing the
+    '''Stop the example by closing the channel and connection and releasing the
     thread. We set a flag here so that we stop scheduling new messages to be
     published. The IOLoop is started because this method is
     invoked by the Try/Catch below when KeyboardInterrupt is caught.
     Starting the IOLoop again will allow the publisher to cleanly
     disconnect from RabbitMQ.
-    """
+    '''
     LOGGER.info('Stopping')
     self._stopping = True
     self.close_channel()
@@ -497,17 +497,17 @@ class SimplePikaTopicPublisher():
     LOGGER.info('Stopped')
 
   def close_connection(self):
-    """This method closes the connection to RabbitMQ."""
+    '''This method closes the connection to RabbitMQ.'''
     LOGGER.info('Closing connection')
     self._closing = True
     self._connection.close()
 
   def producer_heart_beat(self):
-    """This is the callback that is called ever publish_tempo_sec to check to
+    '''This is the callback that is called ever publish_tempo_sec to check to
     see if something is in the thread_queue.  If there are items in this queue
     it schedules other callbacks to send out the messages, and temporarily
     increases its frequecy to deal with queue bursting.
-    """
+    '''
     if self._task_run_event.is_set():
       if self._stopping:
         return
@@ -528,11 +528,11 @@ class SimplePikaTopicPublisher():
           LOGGER.info('Scheduling next output message in %0.6f seconds', self.PUBLISH_FAST_INTERVAL_SEC)
 
   def post_fifo(self, message):
-    """use this to post messages to the network"""
+    '''use this to post messages to the network'''
     self._thread_queue.put(message)
 
   def start_thread(self):
-    """Add a thread so that the run method doesn't steal our program control."""
+    '''Add a thread so that the run method doesn't steal our program control.'''
     self._task_run_event.set()
     self._stopping = False
 
@@ -546,15 +546,33 @@ class SimplePikaTopicPublisher():
     thread.start()
 
   def stop_thread(self):
-    """stop the thread, but keep the connection open.  To close the connection
-    and stop the thread, use the 'stop' api"""
+    '''stop the thread, but keep the connection open.  To close the connection
+    and stop the thread, use the 'stop' api'''
     self._task_run_event.clear()
 
 class PikaTopicPublisher(SimplePikaTopicPublisher):
-  """
-    encryption_function must have this signature: (item, encryption_key)
-    serialization_function must have this signature: (item)
-  """
+  '''This is subclass of SimplePikaTopicPublisher which extends its capabilities.
+
+  It can serialize and encrypt messages before it transmits them.
+  While constructing it, you provide it with a
+  symmetric encryption key, and optional functions for encrypting and
+  serializing messages.
+
+  Example:
+    publisher = \
+      PikaTopicPublisher(
+        amqp_url='amqp://bob:dobbs@192.168.1.69:5672/%2F?connection_attempts=3&heartbeat_interval=3600',
+        routing_key='pub_thread.text',
+        publish_tempo_sec=1.5,
+        exchange_name='sex_change',
+        queue_name='g_queue',
+        encryption_key=b'u3Uc-qAi9iiCv3fkBfRUAKrM1gH8w51-nVU8M8A73Jg='
+      )
+
+    publisher.start_thread()
+    publisher.post_fifo("Publish a Message")
+    publisher.stop_thread()
+  '''
   def __init__(self,
                amqp_url,
                routing_key,
@@ -598,7 +616,7 @@ class PikaTopicPublisher(SimplePikaTopicPublisher):
     else:
       self._serialization_function = serialization_function
 
-  def update_encryption_key(self, encryption_key):
+  def change_encryption_key(self, encryption_key):
     self.stop_thread()
     self._encryption_function = functools.partial(self._sef, encryption_key=encryption_key)
     self.start_thread()
