417d629c0c4848b3f21357cd43364e932d26ec25
Scott Volk <scottvolk@gmail.com>
2018-04-18 09:10:42 -0700

    Working test statechart import of mesh..
    Nothing is connected yet.  Starting the test with an active object.

--- experiment/rabbit/test_mesh.py
+++ experiment/rabbit/test_mesh.py
@@ -1,4 +1,5 @@
 import time
+import mesh
 import uuid
 import random
 from miros.hsm import pp
@@ -7,7 +8,8 @@ from miros.hsm import HsmWithQueues, spy_on
 from miros.activeobject import ActiveObject
 from miros.event import signals, Event, return_status
 
-chart = Factory('test_' + str(uuid.uuid4())[0:5])
+def make_name(pre):
+  return pre + str(uuid.uuid4())[0:5]
 
 def outer_init(chart, e):
   chart.post_fifo(
@@ -35,22 +37,46 @@ def inner_exit(chart, e):
 def inner_to_outer(chart, e):
   return chart.trans(outer)
 
-outer = chart.create(state='outer'). \
-    catch(signal=signals.INIT_SIGNAL, handler=outer_init). \
-    catch(signal=signals.to_inner, handler=outer_to_inner). \
-    to_method()
+@spy_on
+def inner(chart, e):
+  status = return_status.UNHANDLED
+  if(e.signal == signals.ENTRY_SIGNAL):
+    status = inner_entry(chart, e)
+  elif(e.signal == signals.INIT_SIGNAL):
+    status = return_status.HANDLED
+  elif(e.signal == signals.to_outer):
+    status = inner_to_outer(chart, e)
+  elif(e.signal == signals.EXIT_SIGNAL):
+    status = inner_exit(chart, e)
+  else:
+    status, chart.temp.fun = return_status.SUPER, outer
+  return status
 
-inner = chart.create(state='inner'). \
-    catch(signal=signals.ENTRY_SIGNAL, handler=inner_entry). \
-    catch(signal=signals.EXIT_SIGNAL, handler=inner_exit). \
-    catch(signal=signals.to_outer, handler=inner_to_outer). \
-    to_method()
-
-chart.nest(outer, parent=None). \
-    nest(inner, parent=outer)
+@spy_on
+def outer(chart, e):
+  status = return_status.UNHANDLED
+  if(e.signal == signals.ENTRY_SIGNAL):
+    status = return_status.HANDLED
+  elif(e.signal == signals.INIT_SIGNAL):
+    status = outer_init(chart, e)
+  elif(e.signal == signals.to_inner):
+    status = outer_to_inner(chart, e)
+  elif(e.signal == signals.EXIT_SIGNAL):
+    status = return_status.HANDLED
+  else:
+    status, chart.temp.fun = return_status.SUPER, chart.top
+  return status
 
 if __name__ == '__main__':
-  chart.live_trace = True
-  chart.start_at(outer)
-  time.sleep(60)
+  ao = ActiveObject(make_name('active_object'))
+  ao.live_trace = True
+  mn = mesh.MirosNets(miros_object = ao,
+        rabbit_user='bob',
+        rabbit_password='dobbs',
+        mesh_encryption_key=b'u3Uc-qAi9iiCv3fkBfRUAKrM1gH8w51-nVU8M8A73Jg=',
+        routing_key="testing")
+  pp(mn._urls)
+  ao.start_at(outer)
+  time.sleep(10)
+
 
