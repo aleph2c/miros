4438de920d9f23706e9cb19a0e94d658a64a7bbe
Scott Volk <scottvolk@gmail.com>
2019-02-08 19:03:06 -0800

    Style: adding jupyter ignores to .gitignore file

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     py_modules=['miros'],
 
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.15',
+    version='2.0.16',
 
     description='A statechart library for Python',
     long_description=long_description,
