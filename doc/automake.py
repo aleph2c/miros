e1ee5750da9b1c4a3397b0e05de84b58b20aeebf
Scott Volk <scottvolk@gmail.com>
2019-08-01 05:55:20 -0700

    Updating documentation automation.
    The automake.py file was adjusted to get around the watchdog
    trigger-handler-twice bug (93).  Conversion of 'uxf' files to 'svg' and
    'pdf' integrated into script, from the convert_from_uxf.py file.

--- doc/automake.py
+++ doc/automake.py
@@ -12,8 +12,52 @@ from functools import partial
 from collections import deque
 from threading import Thread
 
+from pathlib import Path
+from pathlib import PurePosixPath
+from pathlib import PurePath
+
+Windows_Path_To_UMLet = r'C:\Users\lincoln\Desktop\Umlet\umlet.exe'
+
 with open("automakeconfig.yaml") as f:
   config = yaml.load(f, yaml.FullLoader)
+
+def convert_uxf_to_other_format(filename, convertion_type):
+  '''
+  Uses the umlet command line to convert an umlet drawing into another format.
+
+  Example:
+    # to write './_static/n_ergotic_mongol_1.pdf'
+    convert_uxf_to_other_format(
+      './_static/n_ergotic_mongol_1.uxf', 'pdf')
+
+  '''
+  # umlet throws X11 java errors from Linux (hours of wasted time)
+  # so I use the windows version instead
+  cmd_string = \
+    r"cmd.exe /C '{} -action=convert -format={} -filename={}'". \
+      format(Windows_Path_To_UMLet, convertion_type, filename)
+
+  p = subprocess.Popen(cmd_string,
+                        stdout=subprocess.PIPE,
+                        stdin=subprocess.PIPE,
+                        shell=True)
+  p.communicate()
+  p.wait()
+  path = PurePath.joinpath(
+      PurePosixPath(os.getcwd()),
+      PurePosixPath(filename))
+
+  basename = Path(filename).resolve().stem
+  basepath = str(path.parents[0])
+  try:
+    # old version of umlet mislabel files, here we name them what they should be
+    # named
+    shutil.move(
+      basepath + '/' + basename + ".uxf.{}".format(convertion_type),
+      basepath + '/' + basename + '.{}'.format(convertion_type)
+    )
+  except:
+    pass
 
 class Handler(watchdog.events.PatternMatchingEventHandler):
 
@@ -21,6 +65,8 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
     watchdog.events.PatternMatchingEventHandler.__init__(self,
     patterns=config['change_filters'],
       ignore_directories=True, case_sensitive=False)
+    self.old = None  # needed to work around bug 93 of watchdog
+    self.new = None  # "
 
   def on_modified(self, event):
     def popen_thread(cmd, queue):
@@ -50,28 +96,37 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
         print("the command returned with no output")
 
     if event.src_path.lower().endswith('.rst'):
+      filename = event.src_path
+      statebuf = os.stat(filename)
+      self.new = statebuf.st_mtime
+      if self.old is None or (self.new - self.old) > 0.5:
+        print(event.src_path)
 
+        # make a function which can make a blocking call to the OS
+        to_system = partial(_to_system, queue=deque(maxlen=2))
+
+        # remove the old artifacts from this directory
+        # to_system('make clean')
+
+        # generate the api docs
+        to_system('sphinx-apidoc -f -o ./../miros .')
+
+        # make the new html
+        to_system('make html')
+
+        #if os.path.isdir('./../docs'):
+        #  to_system('rm -rf ./../docs')
+
+        to_system('rsync -a ./_build/html/ ./../docs')
+        to_system('touch ./../docs/.nojekyll')
+        time.sleep(2)
+    elif event.src_path.lower().endswith('.uxf'):
       print(event.src_path)
-
-      # make a function which can make a blocking call to the OS
-      to_system = partial(_to_system, queue=deque(maxlen=2))
-
-      # remove the old artifacts from this directory
-      # to_system('make clean')
-
-      # generate the api docs
-      to_system('sphinx-apidoc -f -o ./../miros .')
-
-      # make the new html
-      to_system('make html')
-
-      #if os.path.isdir('./../docs'):
-      #  to_system('rm -rf ./../docs')
-
-      to_system('rsync -a ./_build/html/ ./../docs')
-      to_system('touch ./../docs/.nojekyll')
-      time.sleep(2)
-
+      if event.event_type == 'modified' or event.event_type == 'created':
+        convert_uxf_to_other_format(event.src_path, 'pdf')
+        convert_uxf_to_other_format(event.src_path, 'svg')
+        print(event.src_path, event.event_type)  # print now only for debug
+      self.old = self.new
   on_created = on_modified
 
 
