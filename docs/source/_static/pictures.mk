096abff29ee2816de4b899de64cd7b2a6ca1db15
Scott Volk <scottvolk@gmail.com>
2020-07-16 08:46:36 -0700

    changing doc build system

--- docs/source/_static/pictures.mk
+++ docs/source/_static/pictures.mk
@@ -5,7 +5,9 @@ $(warning picture make file)
 # 1) make sure umlet.sh is in your path
 # 2) replace line 31 of umlet.sh with:
 # if [! -z "${UMLET_HOME}" ] ; then
-CC=umlet.sh
+#CC="C:\\Users\\lincoln\\Desktop\\Umlet\\Umlet.exe"
+CC  = "$(WINDOWS_UMLET)"
+$(warning $(CC))
 UXF = $(shell find . -name "*.uxf")
 SVG = $(patsubst %.uxf, %.svg,$(UXF))
 PDF = $(patsubst %.uxf, %.pdf,$(UXF))
@@ -19,8 +21,8 @@ all : $(SVG) $(PDF)
 
 %.svg: %.uxf
 > echo $<; \
-> DISPLAY= $(CC) -action=convert -format=svg -filename=$<;
+> cmd.exe /C $(CC) -action=convert -format=svg -filename=$<;
 
 %.pdf: %.uxf
 > echo $<; \
-> DISPLAY= $(CC) -action=convert -format=pdf -filename=$<;
+> cmd.exe /C $(CC) -action=convert -format=pdf -filename=$<;
